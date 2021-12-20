b0c770245a532520f6f2e7961501e2ddcb86f454
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-20 20:36:43 +0200

    updated main project

--- Module3HW5/Module3HW5/Program.cs
+++ Module3HW5/Module3HW5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Module3HW5.Service;
 
 namespace Module3HW5
 {
@@ -6,7 +7,10 @@ namespace Module3HW5
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fileService = new FileService();
+            var files = new string[] { "Files/Hello.txt", "Files/World.txt" };
+            var result = fileService.ReadFilesAsync(files).GetAwaiter().GetResult();
+            Console.WriteLine(result);
         }
     }
 }
