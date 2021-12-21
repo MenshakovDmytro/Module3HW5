440b7d14be0a265c3d1c7b3f0a0fe86221d7e155
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-21 19:22:53 +0200

    added FileService

---
+++ Module3HW5/Module3HW5/FileService.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Module3HW5
+{
+    public class FileService
+    {
+        public async Task<string> ReadFileAsync(string file) => await File.ReadAllTextAsync(file);
+
+        public async Task<string> ReadFilesAsync(string[] paths)
+        {
+            var list = new List<Task<string>>();
+            foreach (var item in paths)
+            {
+                list.Add(ReadFileAsync(item));
+            }
+
+            var result = await Task.WhenAll(list);
+            return string.Join(" ", result);
+        }
+    }
+}
